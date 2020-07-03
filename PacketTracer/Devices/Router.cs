92ac43164afb06c683eb2d74dd0549b62e49c274
Lauri <Rantasalo.Lauri@gmail.com>
2020-07-03 12:49:50 +0300

    Laptop commit, not sure what I did last time

--- PacketTracer/Devices/Router.cs
+++ PacketTracer/Devices/Router.cs
@@ -29,24 +29,35 @@ namespace PacketTracer.Devices
         /// Either route packets here from routers RecievePacket method or from Computers SendPacket
         /// </summary>
         /// <param name="destinationIpAdress"></param>
-        /// <param name="physicalInterface"></param>
+        /// <param name="physicalInterface">source physical interface</param>
         public override void RecievePacket(string destinationIpAdress, PhysicalInterface physicalInterface)
         {
             Debug.WriteLine("Checking routing table");
             string destinationSubnet = destinationIpAdress.Remove(destinationIpAdress.LastIndexOf(".")) + ".0";
+            bool noRoute = true;
             foreach (var routingTableRow in routingTable)
             {
-                if (routingTableRow.subnet == destinationSubnet)
+                (Device aDevice, Device bDevice) = routingTableRow.physicalInterface.connectedCable.SortCableDevices(this);
+
+                
+                
+                if (routingTableRow.nextHop == destinationIpAdress)
                 {
-                    if (routingTableRow.nextHop == destinationIpAdress)
-                    {
-                        Debug.WriteLine("Routing packet to destination ip");
-                        (Device aDevice, Device bDevice) = routingTableRow.physicalInterface.connectedCable.SortCableDevices(this);
-                        // Routes  the packet to next router according to the routing table
-                        bDevice.RecievePacket(destinationIpAdress, routingTableRow.physicalInterface);
-                    }
-                    
+                    Debug.WriteLine("Routing packet to destination ip");
+                    // Routes  the packet to next router according to the routing table
+                    bDevice.RecievePacket(destinationIpAdress, routingTableRow.physicalInterface);
+                    noRoute = false;
+                    break;
+                }// If there is a subnet match to destination subnet and it is not the sending devices address
+                else if (routingTableRow.subnet == destinationSubnet && routingTableRow.nextHop != physicalInterface.ipAddress)
+                {
+                    // Hop to next router
                 }
+            }
+
+            if (noRoute)
+            {
+                Debug.WriteLine("No route found to: " + destinationIpAdress);
             }
             //base.RecievePacket(destinationIpAdress, physicalInterface);
         }
